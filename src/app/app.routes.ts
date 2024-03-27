fd6e47b062cfe4e343a71261e4ca7ac925429cb3
theetha1608 <theerthakunjusss@gmail.com>
2024-03-27 19:31:49 +0530

    Almost finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', 
+        component: HomeComponent
+    },
+    {
+        path:"profile-component",
+        component :ProfileComponent
+    },
+    {
+        path:"about-component",
+        component: AboutComponent
+        
+    },
+];
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+user = {
+    name: 'John Doe',
+    email: 'john@example.com'
+  };
+}
